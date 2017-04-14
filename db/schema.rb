74447a0f410d48760b4ef0b3e81c10198c81893c
silentfl <silentfl@mail.ru>
2017-04-14 20:23:05 +0300

    Update db schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170414092258) do
+ActiveRecord::Schema.define(version: 20170414183000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,12 @@ ActiveRecord::Schema.define(version: 20170414092258) do
     t.integer  "takeoff_time"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "statuses", force: :cascade do |t|
+    t.integer  "aircraft_id"
+    t.integer  "status",      default: 0
+    t.datetime "created_at",              null: false
   end
 
 end
