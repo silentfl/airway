bfd4c95db7f246ea86a01b9c17e636b9738cfc20
silentfl <silentfl@mail.ru>
2017-04-14 20:24:28 +0300

    Statuses created automatically now

--- app/models/aircraft.rb
+++ app/models/aircraft.rb
@@ -1,5 +1,6 @@
 class Aircraft < ActiveRecord::Base
   belongs_to :airtype
+  has_many :statuses, class_name: 'Status'
 
   validates :airtype, presence: true
 
@@ -8,9 +9,17 @@ class Aircraft < ActiveRecord::Base
   scope :in_aeroport, ->{ where(status: Aircraft.statuses[:on_base]) }
   scope :on_takeoff, ->{ where(status: Aircraft.statuses[:waiting]) }
 
+  after_save :create_status
+
   def allow_takeoff
     self.status = :waiting
     self.save!
     TakeoffWorker.perform_async(id)
+  end
+
+  private
+
+  def create_status
+    self.statuses.create!(status: self.status)
   end
 end
